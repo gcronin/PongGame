626d7d02b26a73585f727d56ddfa4bcfc0425498
gcronin <gcronin@seattleacademy.org>
2015-07-12 21:37:22 -0700

    Joysticks Move Both Paddles, Scores Implemented

--- PongGame/src/ponggame/PongGame.java
+++ PongGame/src/ponggame/PongGame.java
@@ -11,52 +11,91 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.lang.Math;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
 
 public class PongGame extends JFrame
 {
     private static final int frameWidth = 500;
     private static final int frameHeight = 500;
     
+    // global variable instances created
+    JLabel player1ScoreLabel;
+    JLabel player2ScoreLabel;
+    JLabel endOfGameField;
+    JButton startButton; 
+    JButton stopButton;
+    Ball ball;
+    Paddle leftPaddle;
+    Paddle rightPaddle;
+    JoystickController readJoystick ;
+    Listener listener;
+    Timer timer;
+    GamePanel gamePanel;
+    
     public static void main(String[] args)
     {
-      JFrame frame = new JFrame("Pong Game");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       PongGame game = new PongGame();
+       game.initializeGame(game);
+    }
+
+    public void initializeGame(PongGame game)
+    {
+      // variable definions
+      ball = new Ball(200, 100, 30, 10);
+      leftPaddle = new Paddle(0, 100, 20, 100, ball);
+      rightPaddle =  new Paddle(460, 100, 20, 100, ball);
+      readJoystick = new JoystickController();
+      player1ScoreLabel = new JLabel("Player 1: 0");
+      player2ScoreLabel = new JLabel("Player 2: 0");
+      endOfGameField = new JLabel("");
+      startButton = new JButton("Start");
+      stopButton =  new JButton("Stop");
       
-      BorderLayout layout = new BorderLayout();
-      frame.setLayout(layout);
+      gamePanel = new GamePanel(ball, leftPaddle, rightPaddle, player1ScoreLabel, player2ScoreLabel, readJoystick, game);
       
+      readJoystick.SetController();
+      
+       
+      // Outer frame for controla
       JPanel settingsPanel = new JPanel();
       settingsPanel.setBackground(Color.YELLOW);
-      
-      JLabel heading = new JLabel("Heading: ");
-      JButton startButton = new JButton("Start");
-      JButton stopButton = new JButton("Stop");
+      settingsPanel.add(player1ScoreLabel);
       settingsPanel.add(startButton);
       settingsPanel.add(stopButton);
-      settingsPanel.add(heading);
+      settingsPanel.add(player2ScoreLabel);
       
-            
-      Ball ball = new Ball(200, 100, 30, 10);
-      Paddle leftPaddle = new Paddle(0, 100, 20, 100, ball);
-      Paddle rightPaddle = new Paddle(460, 100, 20, 100, ball);
-      GamePanel gamePanel = new GamePanel(ball, leftPaddle, rightPaddle, heading);
-      
-      
+      // Inner frame is where the actual game is played
+      JFrame frame = new JFrame("Pong Game"); 
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      BorderLayout layout = new BorderLayout();
+      frame.setLayout(layout);
+      frame.add(settingsPanel, BorderLayout.NORTH);
+      frame.add(gamePanel, BorderLayout.CENTER);
+      frame.setSize(frameWidth, frameHeight);
+      frame.setVisible(true);
+
+      //Listeners
       Listener listener = new Listener(gamePanel);
-      Timer timer = new Timer(50, listener);
+      timer = new Timer(50, listener);
       gamePanel.addMouseMotionListener(listener);
+      gamePanel.add(endOfGameField);
       gamePanel.requestFocusInWindow();
       
       ButtonListener buttonListener = new ButtonListener(startButton, stopButton, timer);
       startButton.addActionListener(buttonListener);
       stopButton.addActionListener(buttonListener);
-      
-            
-      frame.add(settingsPanel, BorderLayout.NORTH);
-      frame.add(gamePanel, BorderLayout.CENTER);
-      frame.setSize(frameWidth, frameHeight);
-      frame.setVisible(true);
-  }
+                  
+    }
+    
+    public void endGame(  )
+    {
+        timer.stop();
+        endOfGameField.setText("GAME OVER!!!");
+        startButton.setText("Play Again");
+    }
 }
 
 class GamePanel extends JPanel
@@ -65,15 +104,23 @@ class GamePanel extends JPanel
     private final Ball ball;
     public Paddle leftPaddle;
     public Paddle rightPaddle;
-    private final JLabel head;
+    private final JLabel scoreLabel1;
+    private final JLabel scoreLabel2;
+    private final JoystickController readJoystick;
+    private int score1 = 0;
+    private int score2 = 0;
+    private final PongGame game;
     
     // constructor
-    public GamePanel(Ball ball, Paddle leftPaddle, Paddle rightPaddle, JLabel head)
+    public GamePanel(Ball ball, Paddle leftPaddle, Paddle rightPaddle, JLabel scoreLabel1, JLabel scoreLabel2, JoystickController readJoystick, PongGame game)
     {
         this.ball = ball;
         this.leftPaddle = leftPaddle;
         this.rightPaddle = rightPaddle;
-        this.head = head;
+        this.scoreLabel1 = scoreLabel1;
+        this.scoreLabel2 = scoreLabel2;
+        this.readJoystick = readJoystick;
+        this.game = game;
     }
     
     // methods
@@ -95,30 +142,23 @@ class GamePanel extends JPanel
     public void animate()
     {
         ball.move();
-        head.setText("heading: " + Math.round(Math.toDegrees(ball.heading)));
-        //System.out.println("Ball: " + ball.xcor + "  Paddle Edge:  " + leftPaddle.width);
-        if( ball.xcor  < leftPaddle.width  )
+        if( ball.xcor < 0 || ball.xcor > getWidth() ) { game.endGame(); }
+        readJoystick.GetYAxesValues();
+        scoreLabel1.setText("Player 1: " + score1);
+        scoreLabel2.setText("Player 2: " + score2);
+        
+        if( ball.xcor - 5  < leftPaddle.width  )  // -5 is a hack which prevents ball from getting "stuck" on the paddle
         {
-            
             if(leftPaddle.overlaps(ball.ycor)) {
-                ball.heading = Math.PI - ball.heading;
-                while(ball.heading > Math.PI ) {
-                    ball.heading -= 2*Math.PI;
-                }
-                while(ball.heading < -Math.PI ) {
-                    ball.heading += 2*Math.PI;
-                }
-                if( Math.abs(ball.heading) > Math.PI / 2)
-                {
-                    ball.heading = Math.PI - ball.heading;
-                }
-           }
+                ball.heading = Math.PI - ball.heading;         
+                score1 += 1;
+            }
         }
-        if(( ball.xcor + ball.ballDiameter)  > getWidth() - rightPaddle.width)
+        if(( ball.xcor + ball.ballDiameter + 5 )  > getWidth() - leftPaddle.width)
         {
-            
             if(rightPaddle.overlaps(ball.ycor)) {
                 ball.heading = Math.PI - ball.heading;
+                score2 += 1;
            }
         }
         // bounce vertically
@@ -127,8 +167,18 @@ class GamePanel extends JPanel
             ball.heading = -ball.heading;
         }
         
-        // right paddle tracks ball
-        rightPaddle.ycor = ball.ycor - rightPaddle.height/2;
+        //leftPaddle moves at speed set by left joystick value
+        leftPaddle.ycor += readJoystick.Y1Value*20; 
+        if(leftPaddle.ycor < 0) { leftPaddle.ycor = 0; }
+        if(leftPaddle.ycor > getHeight()- leftPaddle.height )  { leftPaddle.ycor = getHeight() - leftPaddle.height; }
+        
+        //rightPaddle moves at speed set by right joystick value
+        rightPaddle.ycor += readJoystick.Y2Value*20; 
+        if(rightPaddle.ycor < 0) { rightPaddle.ycor = 0; }
+        if(rightPaddle.ycor > getHeight()- rightPaddle.height )  { rightPaddle.ycor = getHeight() - rightPaddle.height; }
+        
+        //right paddle tracks ball
+        //rightPaddle.ycor = ball.ycor - rightPaddle.height/2;
         
         repaint();
     }
@@ -154,7 +204,7 @@ class Listener implements ActionListener, MouseMotionListener
     
      @Override
     public void mouseMoved(MouseEvent e) {
-        //System.out.println("Mouse moved: " + e.getX() + ", " + e.getY());
+        /*System.out.println("Mouse moved: " + e.getX() + ", " + e.getY());
         if(e.getY() <= gamePanel.leftPaddle.height/2)
         {
            gamePanel.leftPaddle.ycor = 0; 
@@ -166,7 +216,7 @@ class Listener implements ActionListener, MouseMotionListener
         else
         {
             gamePanel.leftPaddle.ycor = e.getY() - gamePanel.leftPaddle.height/2;
-        }
+        }*/
     }
     
     @Override
@@ -237,6 +287,7 @@ class Paddle
     public int width;
     public int height;
     public int ballDiameter;
+    public int ballRadius;
     
     // constructor
     public Paddle(int xcor, int ycor, int width, int height, Ball ball)
@@ -246,11 +297,64 @@ class Paddle
         this.width = width;
         this.height = height;
         this.ballDiameter = ball.ballDiameter;
+        this.ballRadius = ballDiameter/2;
     } 
     
     public boolean overlaps(int ballycor)
     {
-        if( ballycor + ballDiameter/2 > (this.ycor + height ) || ballycor + ballDiameter/2 < (this.ycor ))  { return false; }
+        if( ballycor + ballRadius >= (this.ycor + height ) || ballycor + ballRadius <= (this.ycor ))  { return false; }
         else return true;
     }
+}
+
+class JoystickController
+{
+    public float Y1Value;
+    public float Y2Value;
+       
+    Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+    private final  String controllerType = "Stick";
+    private  Controller logitech = null;
+    
+    public void SetController()
+    {
+            for(int i=0; i < ca.length && logitech == null; i++) {
+                System.out.println(ca[i].getType().toString());
+                if(ca[i].getType().toString().equals(controllerType)) {
+                // Found a controller
+                    logitech = ca[i];
+                    break;
+                }
+            }
+        
+            if(logitech == null) {
+                // Couldn't find a controller
+                System.out.println("Found no desired controller!");
+                System.exit(0);
+            }
+        }
+    
+    public void GetYAxesValues()
+    {
+        logitech.poll();
+        Component[] components = logitech.getComponents();
+                
+                for(int i=0;i<components.length;i++) {
+                    
+                    if(components[i].getName().equals("Z Rotation"))
+                    {
+                        Y2Value = components[i].getPollData();
+                        //deadband
+                        if(Math.abs(Y2Value) < 0.1) { Y2Value = 0; }
+                    }
+                    if(components[i].getName().equals("Y Axis"))
+                    {
+                        Y1Value = components[i].getPollData();
+                        //deadband
+                        if(Math.abs(Y1Value) < 0.1) { Y1Value = 0; }
+                    }
+                } 
+    }            
+               
+    
 }
